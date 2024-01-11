e3aca5077cb5956e4b3a05d7249fbf4c7d4124ba
pratham1324098 <prathamkumar882@gmail.com>
2024-01-11 12:26:21 +0530

    phirst commit

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+const colors = require("colors")
+
+const connectDB = async ()=>{
+    try{
+        await mongoose.connect(process.env.CONNECTION_STRING)
+        console.log(`Connected to Mongodb Database ${mongoose.connection.host}`.bgMagenta.white)
+    }catch(error){
+        console.log(`Mongodb Database Error ${error}`.bgRed.white)
+    }
+}
+module.exports = connectDB
